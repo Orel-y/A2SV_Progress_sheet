0416192db01fa57fa245c422c4ea4e2aa6c7acb0
Yoseph won <yosephwondimu26@gmail.com>
2026-02-02 21:13:49 +0300

    Optimal one after contest analysis

--- contest 2/D_Sereja_and_Dima.py
+++ contest 2/D_Sereja_and_Dima.py
@@ -4,26 +4,27 @@ cards_list = list(map(int, input().split()))
 sereja: int = 0
 dima: int = 0
 turn = True #serajs turn True, and dimas turn False
+left, right = 0, len(cards_list) - 1
 
-while len(cards_list) > 0:
+while left <= right:
     if turn:
-        if cards_list[0] >= cards_list[-1]:
-            sereja += cards_list[0]
-            cards_list = cards_list[1:]
+        if cards_list[left] >= cards_list[right]:
+            sereja += cards_list[left]
+            left += 1
             turn = False
-        elif cards_list[0] <= cards_list[-1]:
-            sereja += cards_list[-1]
-            cards_list.pop()
+        elif cards_list[left] <= cards_list[right]:
+            sereja += cards_list[right]
+            right -= 1
             turn = False
     
     elif not turn:
-        if cards_list[0] >= cards_list[-1]:
-            dima += cards_list[0]
-            cards_list = cards_list[1:]
+        if cards_list[left] >= cards_list[right]:
+            dima += cards_list[left]
+            left += 1
             turn = True
-        elif cards_list[0] <= cards_list[-1]:
-            dima += cards_list[-1]
-            cards_list.pop()
+        elif cards_list[left] <= cards_list[right]:
+            dima += cards_list[right]
+            right -= 1
             turn = True
 
 print (sereja, dima)
