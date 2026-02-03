89fcc91d1ac3c789fed708f6846c3ee18bda4956
Yoseph won <yosephwondimu26@gmail.com>
2026-02-03 11:08:04 +0300

    HR

---
+++ python_basics/string_mutuation.py
@@ -0,0 +1,12 @@
+def mutate_string(string, position, character):
+    str_list = list(string)
+    str_list[position] = character
+    
+    answer = "".join(str_list)
+    return answer
+
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
